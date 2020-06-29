8bddc4724a8dad145555658b4b37059ab976be3c
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-06-30 01:02:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import { set, get } from "idb-keyval";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState("");
+    const [value, setValue] = useState("");
+    function handleData(e) {
+        e.preventDefault();
+        setData(e.target.value);
+    }
+
+    function updateIndexedDB() {
+        set("name", data);
+    }
+
+    function getData() {
+        get("name").then((val) => setValue(val));
+    }
+    return (
+        <div className="App">
+            <div>
+                <input placeholder="send data" onChange={handleData} />
+                <button onClick={updateIndexedDB}>Send</button>
+            </div>
+            <div>
+                <p>{value != "" ? value : null}</p>
+                <button onClick={getData}>Get</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
